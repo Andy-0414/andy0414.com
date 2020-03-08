35a78f94e4421da47208fe79774f3b81ef39348a
Andy0414 <pjh8667@naver.com>
2020-03-08 21:38:19 +0900

    Update content box

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,8 +1,16 @@
 <template>
-	<div class="main">
+	<section class="main">
+		<article class="head">
+			<img src="@/assets/undraw_web_developer_p3e5.svg" class="head__img" />
+			<div class="head__content">
+				<h1>Andy0414</h1>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam aliquam saepe enim reprehenderit ullam, amet fugiat eveniet consequatur ipsa autem qui placeat inventore facilis tenetur earum veritatis mollitia officiis repudiandae!</p>
+			</div>
+		</article>
+
 		<h1 class="color-title">
 			Colors
-			<a href="https://undraw.co/illustrations">(REF)</a>
+			<a href="https://undraw.co/illustrations" target="_blank">(REF)</a>
 		</h1>
 		<section class="color-item">
 			<div class="color-item__color color1"></div>
@@ -16,7 +24,7 @@
 			<div class="color-item__color color3"></div>
 			<div class="color-item__content">#f9a826</div>
 		</section>
-	</div>
+	</section>
 </template>
 
 <script>
@@ -25,10 +33,23 @@ export default {};
 
 <style lang="scss">
 .main {
-	width: 300px;
+	width: 100%;
+	max-width: 1200px;
 	margin: 0 auto;
 	padding: 40px;
 }
+
+.head {
+    width: 100%;
+    display: flex;
+	.head__img {
+		width: 40%;
+    }
+    .head__content{
+        padding: 20px;
+    }
+}
+
 .color-title {
 	font-size: 40px;
 	text-align: center;
