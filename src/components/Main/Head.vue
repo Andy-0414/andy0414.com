abe19ce531274a3befde5faceb92d31f94177dd0
Andy0414 <pjh8667@naver.com>
2020-03-15 01:14:35 +0900

    Update head content

--- src/components/Main/Head.vue
+++ src/components/Main/Head.vue
@@ -3,7 +3,11 @@
 		<img src="@/assets/undraw_web_developer_p3e5.svg" class="head__img" />
 		<div class="head__content">
 			<h1>Andy0414</h1>
-			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam aliquam saepe enim reprehenderit ullam, amet fugiat eveniet consequatur ipsa autem qui placeat inventore facilis tenetur earum veritatis mollitia officiis repudiandae!</p>
+			<p>Full Stack Web Developer</p>
+			<p>
+				GITHUB:
+				<a href="https://github.com/andy-0414">https://github.com/andy-0414</a>
+			</p>
 		</div>
 	</article>
 </template>
