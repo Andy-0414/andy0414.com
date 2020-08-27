1085951074778fd70d0105334b8fa8bb1339bc93
Andy0414 <pjh8667@naver.com>
2020-08-27 10:25:38 +0900

    Update animation

--- src/components/Main/Head.vue
+++ src/components/Main/Head.vue
@@ -1,48 +1,87 @@
 <template>
 	<article class="head">
-		<img src="@/assets/undraw_web_developer_p3e5.svg" class="head__img" alt="profile_image" />
+		<transition name="fade">
+			<img
+				v-if="show"
+				src="@/assets/undraw_web_developer_p3e5.svg"
+				class="head__img"
+				alt="profile_image"
+			/>
+		</transition>
 		<div class="head__content">
-			<h1>Andy0414</h1>
-			<p>Full Stack Web Developer</p>
-			<p class="head__content__icons">
-				<a
-					class="head__content__iconbox"
-					href="mailto:pjh8667@gmail.com"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<span style="display:none">email</span>
-					<i class="head__content__iconbox__icons mdi mdi-email"></i>
-				</a>
-				<a
-					class="head__content__iconbox"
-					href="https://github.com/Andy-0414"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<span style="display:none">github</span>
-					<i class="head__content__iconbox__icons mdi mdi-github"></i>
-				</a>
-				<a
-					class="head__content__iconbox"
-					href="https://www.facebook.com/profile.php?id=100006359646561"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<span style="display:none">facebook</span>
-					<i class="head__content__iconbox__icons mdi mdi-facebook"></i>
-				</a>
-			</p>
+			<transition name="fade">
+				<h1 v-if="show">Andy0414</h1>
+			</transition>
+			<transition name="fade">
+				<p v-if="show">Full Stack Web Developer</p>
+			</transition>
+			<transition name="fade">
+				<p class="head__content__icons" v-if="show">
+					<a
+						class="head__content__iconbox"
+						href="mailto:pjh8667@gmail.com"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<span style="display:none">email</span>
+						<i class="head__content__iconbox__icons mdi mdi-email"></i>
+					</a>
+					<a
+						class="head__content__iconbox"
+						href="https://github.com/Andy-0414"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<span style="display:none">github</span>
+						<i class="head__content__iconbox__icons mdi mdi-github"></i>
+					</a>
+					<a
+						class="head__content__iconbox"
+						href="https://www.facebook.com/profile.php?id=100006359646561"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<span style="display:none">facebook</span>
+						<i class="head__content__iconbox__icons mdi mdi-facebook"></i>
+					</a>
+					<a class="head__content__iconbox" href="tel:010-7207-8667" target="_blank" rel="noreferrer">
+						<span style="display:none">facebook</span>
+						<i class="head__content__iconbox__icons mdi mdi-phone"></i>
+					</a>
+				</p>
+			</transition>
 		</div>
 	</article>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({});
+export default Vue.extend({
+	data() {
+		return {
+			show: false,
+		};
+	},
+	mounted() {
+		setTimeout(() => {
+			this.show = true;
+		}, 200);
+	},
+});
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active {
+	transition: 1s;
+}
+.fade-enter {
+	transform: translateY(20px);
+	opacity: 0;
+}
+.fade-enter-to {
+	transform: translateY(0);
+	opacity: 1;
+}
 .head {
 	text-align: center;
 	.head__img {
@@ -56,10 +95,17 @@ export default Vue.extend({});
 	}
 	.head__content {
 		padding: 20px;
+		h1 {
+			transition-delay: 0.2s;
+		}
 		p {
+			transition-delay: 0.4s;
+
 			margin-top: 10px;
 		}
 		.head__content__icons {
+			transition-delay: 0.6s;
+
 			display: flex;
 			justify-content: space-around;
 			width: 100%;
@@ -68,8 +114,8 @@ export default Vue.extend({});
 				border: none;
 				color: white;
 				outline: none;
-				width: 60px;
-				height: 60px;
+				width: 40px;
+				height: 40px;
 				border-radius: 100%;
 
 				display: flex;
@@ -81,7 +127,7 @@ export default Vue.extend({});
 					background-color: $background-color;
 				}
 				.head__content__iconbox__icons {
-					font-size: 40px;
+					font-size: 30px;
 				}
 			}
 		}
